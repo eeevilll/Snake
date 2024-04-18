67c0d581347aa122bd53782102ed38de195827cf
eee <157961276+wissalyahia@users.noreply.github.com>
2024-04-18 22:40:00 +0200

    Update test_snake-unittest-mock.py

--- GAME/Simple snake game/test_snake-unittest-mock.py
+++ GAME/Simple snake game/test_snake-unittest-mock.py
@@ -1,76 +1,44 @@
 import unittest
-from unittest.mock import patch, Mock
-
-
-from simple_snake import start, update, finish
-
+from unittest.mock import patch
+import random
+import pygame
+from simple_snake import calc_food_position, check_collisions, update
 
 class TestSnakeGame(unittest.TestCase):
-    
-    @patch('simple_snake.pygame.display.update')
-    @patch('simple_snake.pygame.time.delay')
-    @patch('simple_snake.pygame.quit')
-    def test_start(self, mock_quit, mock_delay, mock_update):
-        start()
-        mock_update.assert_called_once()
-        mock_delay.assert_called_once_with(1500)
-        mock_quit.assert_not_called()
-        
-    @patch('simple_snake.pygame.event.get', return_value=[Mock(type=0)])
-    @patch('simple_snake.pygame.display.update')
-    @patch('simple_snake.pygame.quit')
-    def test_update_exit(self, mock_quit, mock_update, mock_get_event):
-        #test case when pygame ouit //////// wissal
-    
-        self.assertEqual(update(), 0)
-        mock_update.assert_called()
-        mock_quit.assert_called_once()
-        
-    @patch('simple_snake.pygame.event.get', return_value=[Mock(type=1, key=Mock(key=0))])
-    @patch('simple_snake.pygame.display.update')
-    @patch('simple_snake.pygame.quit')
-    def test_update_direction_change(self, mock_quit, mock_update, mock_get_event):
-        #test case when pygame keydown //////// wissal
-        self.assertEqual(update(), 0)
-        mock_update.assert_called()
-        mock_quit.assert_not_called()
-        
-    @patch('simple_snake.pygame.event.get', return_value=[Mock(type=1, key=Mock(key=0))])
-    @patch('simple_snake.pygame.display.update')
-    @patch('simple_snake.pygame.quit')
-    def test_update_no_direction_change(self, mock_quit, mock_update, mock_get_event):
-        #test same case but no direction change////wissal
-        self.assertEqual(update(), 0)
-        mock_update.assert_called()
-        mock_quit.assert_not_called()
-        
-    @patch('simple_snake.pygame.event.get', return_value=[Mock(type=1, key=Mock(key=1))])
-    @patch('simple_snake.pygame.display.update')
-    @patch('simple_snake.pygame.quit')
-    def test_update_collision(self, mock_quit, mock_update, mock_get_event):
-        # test when collision /////wissal
-        self.assertEqual(update(), 0)
-        mock_update.assert_called()
-        mock_quit.assert_called_once()
-        
-    @patch('simple_snake.calc_food_position', side_effect=[(0, 0), (0, 0)])
-    @patch('simple_snake.pygame.event.get', return_value=[Mock(type=1, key=Mock(key=0))])
-    @patch('simple_snake.pygame.display.update')
-    @patch('simple_snake.pygame.quit')
-    def test_update_eat_food(self, mock_quit, mock_update, mock_get_event, mock_food_position):
-        # test when my snake eat food //// :)
-        self.assertEqual(update(), 1)
-        mock_update.assert_called()
-        mock_quit.assert_not_called()
-        mock_food_position.assert_called()
-        
-    @patch('simple_snake.pygame.time.delay')
-    @patch('simple_snake.pygame.quit')
-    def test_finish(self, mock_quit, mock_delay):
-        finish(10)
-        mock_delay.assert_called_once_with(1500)
-        mock_quit.assert_called_once()
 
+
+    @patch('random.randrange')
+    def test_calc_food_position(self, mock_randrange):
+      
+        mock_randrange.side_effect = [10, 20]
+        
+     
+        position = calc_food_position(bias=(10, 10))
+        
+   
+        expected_position = (10, 20)
+        self.assertEqual(position, expected_position)
+
+    def test_check_collisions(self):
+ 
+        mock_snake = [[10, 20], [10, 30], [20, 30], [30, 30]]
+        
+
+        self.assertFalse(check_collisions(mock_snake))
+
+        mock_snake.append([10, 20])
+
+        self.assertTrue(check_collisions(mock_snake))
+
+    @patch('simple_snake.quit')
+    def test_update(self, mock_quit):
+
+        with patch('simple_snake.calc_food_position') as mock_calc_food_position:
+            mock_calc_food_position.return_value = (50, 50)
+
+            score = update()
+
+            self.assertEqual(score, 0)
 
 if __name__ == '__main__':
     unittest.main()
